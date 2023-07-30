647c1b8bbc75cd08734b0ba11109e2dc80dbacac
ny-Sopheaktra <60089537+ny-Sopheaktra@users.noreply.github.com>
2023-07-30 22:38:19 +0700

    Reponsive commit

--- js/script.js
+++ js/script.js
@@ -6,3 +6,10 @@ function changeBackgroundColor(color) {
   const sec = document.querySelector(".sec");
   sec.style.background = color;
 }
+
+function toggleMenu() {
+  const menuToggle = document.querySelector('.toggle');
+  const navigation = document.querySelector('.navigation');
+  menuToggle.classList.toggle('active');
+  navigation.classList.toggle('active');
+}
